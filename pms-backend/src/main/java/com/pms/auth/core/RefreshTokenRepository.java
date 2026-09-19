ff7e48689464257e9e0ffbe980cb68297fca17e3
Martin <martinnevexis99@gmail.com>
2026-09-19 19:06:04 +0300

    Add missed @Repository to repos

--- pms-backend/src/main/java/com/pms/auth/core/RefreshTokenRepository.java
+++ pms-backend/src/main/java/com/pms/auth/core/RefreshTokenRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
     @Query("SELECT rt FROM RefreshToken rt JOIN FETCH rt.user WHERE rt.tokenHash = :tokenHash")
     Optional<RefreshToken> findByTokenHashFetchUser(@Param("tokenHash") String tokenHash);
